281e7be45440cb424a6b2eb37720f026a97b3fcb
SmitMaruti <smit.patel@marutitech.com>
2022-12-11 15:35:11 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `prismic`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
